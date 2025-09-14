54952bb6d2cf2b4eebc4c3c563665c65a20a91ea
Dnreikronos <dnreikronos2@gmail.com>
2025-09-14 16:49:33 -0300

    feature: set the provider directly into the Streamer model

--- internal/model/streamer.go
+++ internal/model/streamer.go
@@ -3,18 +3,21 @@ package model
 import (
 	"time"
 
+	"github.com/Dnreikronos/givememoney.fun-backend/internal/utils"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Streamer struct {
-	ID        uuid.UUID `json:"id" gorm:"type:uuid;primaryKey"`
-	Name      string    `json:"name,omitempty"`
-	Email     string    `json:"email"`
-	WalletID  uuid.UUID `json:"wallet_id" gorm:"type:uuid;not null;index"`
-	Wallet    Wallet    `json:"wallet" gorm:"foreignKey:WalletID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+	ID         uuid.UUID              `json:"id" gorm:"type:uuid;primaryKey"`
+	Provider   utils.StreamerProvider `json:"provider" gorm:"not null"`
+	ProviderID string                 `json:"provider_id" gorm:"not null; index:idx_provider_id"`
+	Name       string                 `json:"name,omitempty"`
+	Email      string                 `json:"email"`
+	WalletID   uuid.UUID              `json:"wallet_id" gorm:"type:uuid;not null;index"`
+	Wallet     Wallet                 `json:"wallet" gorm:"foreignKey:WalletID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	CreatedAt  time.Time              `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt  time.Time              `json:"updated_at" gorm:"autoUpdateTime"`
 }
 
 func (s *Streamer) BeforeCreate(tx *gorm.DB) (err error) {
