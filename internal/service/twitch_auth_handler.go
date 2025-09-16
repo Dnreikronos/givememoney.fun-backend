f41c839b073b0adb64a3652d4197011ba54608d8
Dnreikronos <dnreikronos2@gmail.com>
2025-09-15 22:12:26 -0300

    feature: create function NewTwitchAuthHandler(0

--- internal/service/twitch_auth_handler.go
+++ internal/service/twitch_auth_handler.go
@@ -9,6 +9,13 @@ type TwitchAuthHandler struct {
 	clientSecret string
 	redirectURL  string
 }
+func NewTwitchAuthHandler() *TwitchAuthHandler {
+	return &TwitchAuthHandler{
+		clientID:     os.Getenv("TWITCH_CLIENT_ID"),
+		clientSecret: os.Getenv("TWITCH_CLIENT_SECRET"),
+		redirectURL:  os.Getenv("TWITCH_REDIRECT_URL"),
+	}
+}
 func generateState() string {
 	b := make([]byte, 32)
 	rand.Read(b)
