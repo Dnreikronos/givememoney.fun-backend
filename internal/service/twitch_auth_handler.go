d9123b15e59ce82bf821ddccfc9d21ab508eb70b
Dnreikronos <dnreikronos2@gmail.com>
2025-09-15 22:12:44 -0300

    feature: create function GenerateAuthURL()

--- internal/service/twitch_auth_handler.go
+++ internal/service/twitch_auth_handler.go
@@ -16,6 +16,17 @@ func NewTwitchAuthHandler() *TwitchAuthHandler {
 		redirectURL:  os.Getenv("TWITCH_REDIRECT_URL"),
 	}
 }
+func (t *TwitchAuthHandler) GenerateAuthURL() string {
+	state := generateState()
+	params := url.Values{
+		"client_id":     {t.clientID},
+		"redirect_uri":  {t.redirectURL},
+		"response_type": {"code"},
+		"scope":         {"user:read:email"},
+		"state":         {state},
+	}
+	return "https://id.twitch.tv/oauth2/authorize?" + params.Encode()
+}
 func generateState() string {
 	b := make([]byte, 32)
 	rand.Read(b)
