88e6fc9402d8a987d78230623cac88f7e9f337ed
Dnreikronos <dnreikronos2@gmail.com>
2025-09-16 21:49:42 -0300

    feature: create GetUser function

---
+++ internal/service/twitch_provider.go
@@ -0,0 +1,12 @@
+func (t *TwitchProvider) GetUser(ctx context.Context, token string) (utils.ProviderUser, error) {
+	user, err := t.handler.GetUser(ctx, token)
+	if err != nil {
+		return utils.ProviderUser{}, err
+	}
+
+	return utils.ProviderUser{
+		ID:    user.ID,
+		Name:  user.DisplayName,
+		Email: user.Email,
+	}, nil
+}
