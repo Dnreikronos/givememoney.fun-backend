9a2bdc2ab823ba97268f1da0b85d336b90e50b44
Dnreikronos <dnreikronos2@gmail.com>
2025-09-18 21:45:54 -0300

    feature: create AuthController struct and NewAuthController function

--- internal/controller/auth_controler.go
+++ internal/controller/auth_controler.go
@@ -1,3 +1,20 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/Dnreikronos/givememoney.fun-backend/internal/service"
+	"github.com/Dnreikronos/givememoney.fun-backend/internal/utils"
+	"github.com/gin-gonic/gin"
+)
+
+type AuthController struct {
+	authService *service.AuthService
+}
+
+func NewAuthController(authService *service.AuthService) *AuthController {
+	return &AuthController{authService: authService}
+}
 
 func (c *AuthController) TwitchLogin(ctx *gin.Context) {
 	authURL, err := c.authService.GetAuthURL(utils.ProviderTwitch)
