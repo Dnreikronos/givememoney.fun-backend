8a772784de5f102b65c012fb85183336c4de804d
Dnreikronos <dnreikronos2@gmail.com>
2025-10-02 22:47:04 -0300

    refactor: create specific file for migrations

---
+++ internal/database/connection/migration.go
@@ -0,0 +1,24 @@
+package connection
+
+import (
+	"log"
+
+	"github.com/Dnreikronos/givememoney.fun-backend/internal/model"
+	"gorm.io/gorm"
+)
+
+func RunMigration(db *gorm.DB) {
+	CreateTable(db)
+}
+func CreateTable(db *gorm.DB) {
+	err := db.AutoMigrate(
+		&model.Wallet{},
+		&model.Streamer{},
+		&model.Session{},
+		&model.RefreshToken{},
+	)
+
+	if err != nil {
+		log.Fatal("Failed to migrate database:", err)
+	}
+}
