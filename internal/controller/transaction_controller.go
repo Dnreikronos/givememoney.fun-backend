ead44ec8b9c2413e9977af10bb21e2bffad4cd0f
Dnreikronos <dnreikronos2@gmail.com>
2025-11-09 14:37:47 -0300

    feature: create Create function on the TransactionController

--- internal/controller/transaction_controller.go
+++ internal/controller/transaction_controller.go
@@ -18,3 +18,21 @@ func NewTransactionController(transactionService *service.TransactionService) *T
 		transactionService: transactionService,
 	}
 }
+
+func (c *TransactionController) Create(ctx *gin.Context) {
+	var req model.TransactionRequest
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	AddressToID, _ := ctx.Get("address_to_id")
+	transaction, err := c.transactionService.Create(ctx, AddressToID.(uuid.UUID), &req)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, transaction)
+}
