c8d9aa97af474f4c55fb9f32ff8aab6fc868852a
Dnreikronos <dnreikronos2@gmail.com>
2025-11-09 15:04:09 -0300

    feature: create the Transaction struct inside of the auth_responses

--- internal/dto/auth_responses.go
+++ internal/dto/auth_responses.go
@@ -1,6 +1,8 @@
 package dto
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 )
 
@@ -13,19 +15,30 @@ type AuthResponse struct {
 }
 
 type UserInfo struct {
-	ID         uuid.UUID `json:"id"`
-	Name       string    `json:"name"`
-	Email      string    `json:"email"`
-	Provider   string    `json:"provider"`
-	ProviderID string    `json:"provider_id"`
-	AvatarURL  string    `json:"avatar_url,omitempty"`
-	Wallets    []Wallet  `json:"wallets,omitempty"`
+	ID           uuid.UUID     `json:"id"`
+	Name         string        `json:"name"`
+	Email        string        `json:"email"`
+	Provider     string        `json:"provider"`
+	ProviderID   string        `json:"provider_id"`
+	AvatarURL    string        `json:"avatar_url,omitempty"`
+	Wallets      []Wallet      `json:"wallets,omitempty"`
+	Transactions []Transaction `json:"transactions,omitempty"`
 }
 
 type Wallet struct {
 	ID             uuid.UUID `json:"id"`
 	WalletProvider string    `json:"wallet_provider"`
 	WalletAddress  string    `json:"wallet_address"`
+}
+
+type Transaction struct {
+	ID          uuid.UUID `json:"id"`
+	Amount      string    `json:"amount"`
+	TxHash      string    `json:"tx_hash"`
+	AddressFrom string    `json:"address_from"`
+	Message     string    `json:"message"`
+	CreatedAt   time.Time `json:"created_at"`
+	Status      string    `json:"status"`
 }
 
 type ErrorResponse struct {
