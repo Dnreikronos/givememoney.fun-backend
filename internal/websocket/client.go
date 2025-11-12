1e63087cdbf5f19f56a09408a244a34de5e5e34e
Dnreikronos <dnreikronos2@gmail.com>
2025-11-12 20:15:47 -0300

    feature: create ReadBump() client function

--- internal/websocket/client.go
+++ internal/websocket/client.go
@@ -13,3 +13,20 @@ type Client struct {
 	send       chan interface{}
 	StreamerID uuid.UUID
 }
+
+func (c *Client) ReadBump() {
+	defer func() {
+		c.hub.unregister <- c
+		c.conn.Close()
+	}()
+	c.conn.SetReadDeadline(time.Now().Add(60 * time.Second))
+	c.conn.SetPongHandler(func(string) error {
+		c.conn.SetReadDeadline(time.Now().Add(60 * time.Second))
+		return nil
+	})
+	for {
+		if _, _, err := c.conn.ReadMessage(); err != nil {
+			break
+		}
+	}
+}
