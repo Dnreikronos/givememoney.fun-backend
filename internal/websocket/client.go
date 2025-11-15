5f69493d93c2b26c3053835181f65852b8be6002
Dnreikronos <dnreikronos2@gmail.com>
2025-11-15 17:56:11 -0300

    refactor: setup struct with Highercase

--- internal/websocket/client.go
+++ internal/websocket/client.go
@@ -8,24 +8,24 @@ import (
 )
 
 type Client struct {
-	hub        *Hub
-	conn       *websocket.Conn
-	send       chan interface{}
+	Hub        *Hub
+	Conn       *websocket.Conn
+	Send       chan interface{}
 	StreamerID uuid.UUID
 }
 
 func (c *Client) ReadPump() {
 	defer func() {
-		c.hub.unregister <- c
-		c.conn.Close()
+		c.Hub.Unregister <- c
+		c.Conn.Close()
 	}()
-	c.conn.SetReadDeadline(time.Now().Add(60 * time.Second))
-	c.conn.SetPongHandler(func(string) error {
-		c.conn.SetReadDeadline(time.Now().Add(60 * time.Second))
+	c.Conn.SetReadDeadline(time.Now().Add(60 * time.Second))
+	c.Conn.SetPongHandler(func(string) error {
+		c.Conn.SetReadDeadline(time.Now().Add(60 * time.Second))
 		return nil
 	})
 	for {
-		if _, _, err := c.conn.ReadMessage(); err != nil {
+		if _, _, err := c.Conn.ReadMessage(); err != nil {
 			break
 		}
 	}
@@ -35,19 +35,19 @@ func (c *Client) WritePump() {
 	ticker := time.NewTicker(54 * time.Second)
 	defer func() {
 		ticker.Stop()
-		c.conn.Close()
+		c.Conn.Close()
 	}()
 	for {
 		select {
 
-		case message, ok := <-c.send:
+		case message, ok := <-c.Send:
 			if !ok {
-				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				c.Conn.WriteMessage(websocket.CloseMessage, []byte{})
 				return
 			}
-			c.conn.WriteJSON(message)
+			c.Conn.WriteJSON(message)
 		case <-ticker.C:
-			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+			if err := c.Conn.WriteMessage(websocket.PingMessage, nil); err != nil {
 				return
 			}
 		}
