d4216ce81532524a1ba9795389e7422e99bcd1e7
Dnreikronos <dnreikronos2@gmail.com>
2025-11-15 17:41:01 -0300

    feature: create WritePump() function to write the websocket message

--- internal/websocket/client.go
+++ internal/websocket/client.go
@@ -30,3 +30,26 @@ func (c *Client) ReadPump() {
 		}
 	}
 }
+
+func (c *Client) WritePump() {
+	ticker := time.NewTicker(54 * time.Second)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
+	for {
+		select {
+
+		case message, ok := <-c.send:
+			if !ok {
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+			c.conn.WriteJSON(message)
+		case <-ticker.C:
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
