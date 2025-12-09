37c0ae2a29f50f2982a95d25bb41bcd2bff3a807
Dnreikronos <dnreikronos2@gmail.com>
2025-12-09 09:26:19 -0300

    refactor: omit streamer struct

--- internal/model/wallet.go
+++ internal/model/wallet.go
@@ -21,7 +21,7 @@ type Wallet struct {
 	WalletProvider utils.WalletProvider `json:"wallet_provider" gorm:"not null;type:varchar(50)"`
 	WalletAddress  string               `json:"wallet_address" gorm:"not null;size:64;unique"`
 	StreamerID     uuid.UUID            `json:"streamer_id" gorm:"type:uuid;not null;index"`
-	Streamer       Streamer             `json:"streamer" gorm:"foreignKey:StreamerID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Streamer       Streamer             `json:"-" gorm:"foreignKey:StreamerID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	CreatedAt      time.Time            `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt      time.Time            `json:"updated_at" gorm:"autoUpdateTime"`
 }
